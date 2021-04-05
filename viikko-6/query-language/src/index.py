ba7c5d2bc5752868de6543072867114207d08ef5
Patrik Keinonen <patrik.keinonen@helsinki.fi>
2021-04-05 19:26:57 +0300

    tehtävä 3

--- viikko-6/query-language/src/index.py
+++ viikko-6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not
+from matchers import And, HasAtLeast, PlaysIn, Not, Or
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
@@ -13,10 +13,19 @@ def main():
     #     PlaysIn("PHI")
     # )
 
+    # matcher = And(
+    #     Not(HasAtLeast(1, "goals")),
+    #     PlaysIn("NYR")
+    # )
+
     matcher = And(
-    Not(HasAtLeast(1, "goals")),
-    PlaysIn("NYR")
-)
+        HasAtLeast(50, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
+    )
 
     for player in stats.matches(matcher):
         print(player)
